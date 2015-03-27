3ee450badfe089c86ddd92ddb859f9ae49f1ab52
Mark Natera <marknatera@gmail.com>
2015-03-27 15:55:37 -0400

    Test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,23 @@
 class PagesController < ApplicationController
+
+  def index
+    @contact = Contact.new
+  end
+
+
+  def create_contact
+    @contact = Contact.create(contact_params)
+    if @contact.save
+      redirect_to thanks_path
+    end
+  end
+
+
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :company, :email, :phone, :quantity, :capacity, :notes)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get '/thanks' => 'pages#thanks'
+  post '/contact' => 'pages#create_contact', as: :contacts
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
