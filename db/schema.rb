cd943070a6ba59ea171680f8430c1dd50faed50f
Mark Natera <marknatera@gmail.com>
2015-05-01 11:27:06 -0400

    Migrated campaign2 and installed simple_form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328180200) do
+ActiveRecord::Schema.define(version: 20150501152433) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,19 @@ ActiveRecord::Schema.define(version: 20150328180200) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "campaign2s", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "company"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "quantity"
+    t.string   "capacity"
+    t.text     "notes"
+    t.string   "style_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "full_name"
